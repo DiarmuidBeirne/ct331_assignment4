4ac440f260dd1839d1ce3e47e37865ca6f8bb5ac
Diarmuid Beirne <d.beirne.8@gmail.com>
2017-11-16 13:32:54 +0000

    Complete

--- assignment.pl
+++ assignment.pl
@@ -1,7 +1,24 @@
 %Definitions for isElementInList(El, List)
 
+isElementInList(El, [El | _]).
+
+isElementInList(El, [_ | T]) :- isElementInList(El, T).
+
+
+
 %Definitions for reverseList(List, ReversedList)
 
 %insertElementIntoListEnd(El, List, NewList)
 
 %Definitions for mergeLists(List1, List2, Merged)
+
+mergeLists([], MyList2, MyList2).
+
+mergeLists([H | T], MyList2, [H | Merged]) :- mergeLists(T, MyList2, Merged).
+
+reverseList([], []).
+
+reverseList([H | T], RList) :- mergeLists(MinList, [H], RList), reverseList(T, MinList).
+
+insertElementIntoListEnd(El, MyList1, NewList) :- mergeLists(MyList1, [El], NewList).
+
